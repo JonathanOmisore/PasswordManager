e8b2c87f40dc86444b372730656eeaa93f73ed44
comprogger <godzillamario@gmail.com>
2016-06-03 22:23:04 -0400

    Update pass.py

--- pass.py
+++ pass.py
@@ -6,7 +6,7 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 import base64
 f = open("/home/jonathan/Dropbox/abcdefg", "a")
-key=b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+key='' //enter key
 cipher = AES.new(key,AES.MODE_CFB, '1234567890123456')
 decryptor = AES.new(key, AES.MODE_CFB, '9348493023950485')
 def checkargumentistoolong(argument):
